e66321701fd9d6a138e4b1548679b4a6a8e0bcae
alexandrajimenezc <artesalexj@gmail.com>
2020-05-31 23:28:06 +0200

    configuración de webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path= require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+//configuración de emtrada y salida
+module.exports ={
+    entry: './src/app/index.js',
+    output:{
+        path: path.join(__dirname,'dist'),
+        filename:'bundle.js'
+    },
+    devServer:{
+        port: 3000
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/ ,
+                use: ['style-loader' , 'css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
